5954f038674cc46f857df59cea175206798928de
Seyyed Ali Akhavani <sa.akhavani@gmail.com>
2017-04-17 15:06:00 +0430

    Add some configs

--- app/app.js
+++ app/app.js
@@ -4,40 +4,71 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
+var session = require('express-session');
 var routes = require('./routes/index');
-var users = require('./routes/users');
+
+var mongoose = require('mongoose');
+var passport = require('passport');
+var flash = require('connect-flash');
+
+// Configs
+var configDB = require('./config/database.js');
+
 
 var app = express();
 
+// Database
+mongoose.connect(configDB.url);
+mongoose.connection.on('connected', function () {
+  console.log('Connected to Database!');
+});
+
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
+app.set('view engine', 'ejs');
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+
+
+// Passport
+app.use(session({
+  secret: 'myNameIsAkhavani',
+  resave: false,
+  saveUninitialized: true,
+})); // session secret
+app.use(passport.initialize());
+app.use(passport.session()); // persistent login sessions
+app.use(flash()); // use connect-flash for flash messages stored in session
+
+
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+
+// Routes
 app.use('/', routes);
-app.use('/users', users);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   var err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
+
+
 
 // error handlers
 
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
+  app.use(function (err, req, res, next) {
     res.status(err.status || 500);
     res.render('error', {
       message: err.message,
@@ -48,7 +79,7 @@ if (app.get('env') === 'development') {
 
 // production error handler
 // no stacktraces leaked to user
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   res.status(err.status || 500);
   res.render('error', {
     message: err.message,
--- app/routes/index.js
+++ app/routes/index.js
@@ -1,9 +1,60 @@
 var express = require('express');
+var passport = require('passport');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+  res.render('index', {
+    title: 'Index'
+  });
 });
+
+// GET login page
+router.get('/login', function (req, res, next) {
+  res.render('login', {
+    title: 'Login',
+    message: req.flash('loginMessage')
+  });
+});
+
+// GET signup page
+router.get('/signup', function (req, res, next) {
+  res.render('signup', {
+    title: 'Sign Up',
+    message: req.flash('signupMessage')
+  });
+});
+
+// GET profile page
+router.get('/profile', isLoggedIn, function (req, res, next) {
+  res.render('profile', {
+    title: 'Sign Up',
+    user: req.user
+  });
+});
+
+// GET logout
+router.get('/logout', function (req, res, next) {
+  res.logout();
+  res.redirect('/');
+});
+
+// POST login
+router.post('/login', passport.authenticate('local', {
+  successRedirect: '/',
+  failureRedirect: '/login',
+  failureFlash: 'Invalid Username or Password'
+}), function (req, res) {
+
+  // the authenticated user is now in req.user
+  console.log(req.user.username);
+});
+
+function isLoggedIn(req, res, next) {
+  if (req.isAuthenticated())
+    return next;
+  res.redirect('/');
+}
+
 
 module.exports = router;
